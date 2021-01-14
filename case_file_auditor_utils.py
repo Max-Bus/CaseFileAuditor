6943db13a51672f34e7ba60fb0e8779b979341c6
vnethrapalli <34689626+vnethrapalli@users.noreply.github.com>
2021-01-14 15:12:18 -0500

    added optional param

--- case_file_auditor_utils.py
+++ case_file_auditor_utils.py
@@ -2,7 +2,7 @@ import docx
 
 # filename: name of file to be read
 # scrub_list: list of words to removed from document
-def read_docx_text(filename, scrub_list):
+def read_docx_text(filename, scrub_list=None):
     document = docx.Document(filename)  # creating word reader object.
     data = ''
 
