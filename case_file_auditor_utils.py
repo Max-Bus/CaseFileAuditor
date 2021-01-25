061949b8989ba8057555a6d1f5eca730996b6bbb
vnethrapalli <34689626+vnethrapalli@users.noreply.github.com>
2021-01-25 14:26:59 -0500

    bag of words?

--- case_file_auditor_utils.py
+++ case_file_auditor_utils.py
@@ -1,4 +1,7 @@
 import docx
+import os
+import re
+import pandas as pd
 
 # filename: name of file to be read
 # scrub_list: list of words to removed from document
@@ -16,6 +19,25 @@ def read_docx_text(filename, scrub_list=None):
 
 # can be used to read .txt and .doc file extensions
 # other formats may return some sort of unicode
-def read_file_contexts(filename, scrub_list=None):
+def read_file_contents(filename, scrub_list=None):
     with open(filename, 'r') as file:
         return file.read()
+
+def get_words_goodie_bag(folder_directory):
+    bag = ''
+    for subdir, dirs, files in os.walk(folder_directory):
+        for file in files:
+            if re.search(r'.docx$', file):
+                bag += read_docx_text(file)
+
+            elif re.search(r'.txt$', file):
+                bag += read_file_contents(file)
+
+            elif re.search(r'.csv$', file):
+                df = pd.read_csv(file)
+
+                # concat strings in matrix
+                text = ''.join([''.join(row) for row in df.values])
+
+                # add column names
+                text += ''.join(df.columns)
