fc2ea8d760941d0d9b3a2c66d2bf818d812206db
vnethrapalli <34689626+vnethrapalli@users.noreply.github.com>
2021-04-09 22:02:26 -0400

    reformatted injury types file, checked for their occurrance in the given document (should be optimized at some point)

--- pyexe/eletest.py
+++ pyexe/eletest.py
@@ -28,15 +28,45 @@ print("test 2")
 
 # instead of console, write contents to file
 
+# read in injury types from file
+possible_injuries = []
+with open('../data/injury-types.txt', 'r') as file:
+    for categ in file.read().lower().split(';'):
+        if '|' in categ:
+            possible_injuries += categ.split('|')
+        else:
+            possible_injuries += categ
+
+# store injury words and their frequency to rank by importance
+potential_key_words = dict(zip(possible_injuries, [0 for i in range(len(possible_injuries))]))
+
 # todo determine how to name the output file
 try:
     with open(args[2] + '\prediction-test.txt', 'w') as file:
-        for concept in extract_keywords(folder_as_document_list(args[1])):
+        folder_doc_list = folder_as_document_list(args[1])
+
+        # look for injury type words and store counts
+        for docs in folder_doc_list:
+            docs = docs.lower()
+            for word in docs.split():
+                if word in possible_injuries:
+                    potential_key_words[word] = potential_key_words[word] + 1
+
+        for concept in extract_keywords(folder_doc_list):
             file.write(','.join(concept) + '\n')
 
         file.write('\n')
-        for concept in extract_keywords(folder_as_document_list(args[1]), 'count'):
+        for concept in extract_keywords(folder_doc_list, 'count'):
             file.write(','.join(concept) + '\n')
+
+
+        file.write('\n')
+        file.write('The following words may describe and/or are related to the injury:\n')
+
+        sorted_frequencies = {key : val for key, val in sorted(potential_key_words.items(), key=lambda item: item[1])}
+        for key, value in sorted_frequencies.items():
+            file.write(f'{key}: {value} occurrences\n')
+
 
 except Exception as e:
     # Just print(e) is cleaner and more likely what you want,
